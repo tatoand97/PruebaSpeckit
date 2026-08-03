b85e668faa0014a95ba3babc36cbdea6bccdcdeb
Ricardo Andres Leyva <ricardoandres9728@hotmail.com>
2026-08-03 08:30:26 -0500

    Remove feature specification and related documents for "Registrar solicitud de contacto" module

--- PoCFinal/Modules/ContactRequests/Application/Abstractions/IContactRequestRepository.cs
+++
@@ -1,8 +0,0 @@
-using ContactRequests.Domain;
-
-namespace ContactRequests.Application.Abstractions;
-
-public interface IContactRequestRepository
-{
-    Task AddAsync(ContactRequest contactRequest, CancellationToken cancellationToken);
-}
--- PoCFinal/Modules/ContactRequests/Application/RegisterContactRequest/RegisterContactRequestResult.cs
+++
@@ -1,6 +0,0 @@
-namespace ContactRequests.Application.RegisterContactRequest;
-
-public sealed record RegisterContactRequestResult(
-    Guid Id,
-    DateTimeOffset CreatedAt,
-    string Status);
